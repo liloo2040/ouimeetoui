964035d6b37decbeab0f1f2fe1ffbd544743f5aa
liloo2040 <e.dufour.89@gmail.com>
2018-06-02 13:08:07 +0200

    adding title and html core

--- view/Profil.php
+++ view/Profil.php
@@ -1,12 +1,13 @@
-<!doctype html>
-<html>
-
+<!DOCTYPE html>
+<html lang="fr">
 <head>
-
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Aperçu du profil</title>
 </head>
 <body>
+
+<h2>Aperçu du profil</h2>
     
-
-
 </body>
 </html>
---
+++ view/ProfilEdit.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Edition du Profil</title>
+</head>
+<body>
+
+<h2>Edition du Profil</h2>
+    
+</body>
+</html>
